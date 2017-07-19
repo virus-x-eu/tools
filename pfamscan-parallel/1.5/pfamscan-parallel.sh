0348e8ada295cbd1ba8c166f8e462e9814865d2d
Christian Henke <maitai@users.noreply.github.com>
2017-07-19 15:17:23 +0200

    fix

--- pfamscan-parallel/1.5/pfamscan-parallel.sh
+++ pfamscan-parallel/1.5/pfamscan-parallel.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cat ${1} | parallel --gnu -j ${2} --recstart '>' --pipe --cat pfam_scan.pl -fasta {} -dir ${3}
+cat ${1} | parallel --gnu -j ${2} --block 100K --recstart '>' --pipe --cat pfam_scan.pl -fasta {} -dir ${3}
