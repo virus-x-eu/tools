f677a37cb744592eadd2e89e673d8e34600c48e7
Christian Henke <maitai@users.noreply.github.com>
2017-10-26 15:55:29 +0200

    fix

--- ena-fastq-download/0.2.1/ena-fastq-download.py
+++ ena-fastq-download/0.2.1/ena-fastq-download.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description='''
 Download fastq files from the European Nucleotide Archive (ENA) to the current directory.
 ''')
 parser.add_argument("--run-accession", dest='run_accession', required=True, help='The run accession ID, e.g. ERR0123456')
-parser.add_argument("--size-limit", dest='size_limit', type=int, default=0, required=False, help='Files are only downloaded if the SRA file size (not FASTQ) in bytes is below this limit.')
+parser.add_argument("--size-limit", dest='size_limit', type=int, default=0, required=False, help='Files are only downloaded if the SRA file size (not FASTQ) in mebibytes is below this limit.')
 args = parser.parse_args()
 
 raw_url_template = 'https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=%s&result=read_run'
@@ -21,7 +21,7 @@ fastq_urls = data['fastq_ftp'][0]
 wget = ['/usr/bin/wget', '-nc', '--progress=dot:giga']
 
 
-if args.size_limit == 0 or data['sra_bytes'][0] < args.size_limit:
+if args.size_limit == 0 or data['sra_bytes'][0] < args.size_limit*1024*1024:
   # check if tsv field was not empty (pandas converts empty fields to NaN which is != str)
   # we also require paired ends in separate files (_1 and _2)
   if type(fastq_urls) is str and '_1.fastq.gz' in fastq_urls and '_2.fastq.gz' in fastq_urls:
