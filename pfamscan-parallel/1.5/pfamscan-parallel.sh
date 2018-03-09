a8950a2203a039ef87e1d168ece3cb90ef5e5e0c
Christian Henke <maitai@users.noreply.github.com>
2018-03-09 13:39:48 +0100

    fix pfam_scan (use 1 cpu per invocation)

--- pfamscan-parallel/1.5/pfamscan-parallel.sh
+++ pfamscan-parallel/1.5/pfamscan-parallel.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cat ${1} | parallel --gnu -j ${2} --block ${3} --no-notice --recstart '>' --pipe --cat pfam_scan.pl -as -fasta {} -dir ${4}
+cat ${1} | parallel --gnu -j ${2} --block ${3} --no-notice --recstart '>' --pipe --cat pfam_scan.pl -cpu 1 -as -fasta {} -dir ${4}
