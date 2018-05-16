4e42c60eb035dc7feceea794708399f9359f028e
Christian Henke <maitai@users.noreply.github.com>
2018-05-16 17:00:16 +0200

    WIP

--- merge-annotations/0.1/merge-annotations.py
+++ merge-annotations/0.1/merge-annotations.py
@@ -10,10 +10,13 @@ import sys
 import time
 
 MIN_ALIGNMENT_LENGTH = 15
-NUMBER_OF_TOP_BLAST_HITS = 10
+NUMBER_OF_TOP_HITS_TO_KEEP = 10
 
 PFAM_IDMAP_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam31.0/database_files/pfamA.txt.gz'
 PDB_IDMAP_URL = 'ftp://ftp.wwpdb.org/pub/pdb/derived_data/index/compound.idx'
+
+TDM_SINGLE_FIELD_MAP = [('nice_name', 'label'), ('seq_count', 'seq_count'), ('structure_count', 'structure_count')]
+TDM_MULTI_FIELD_MAP = [('ec_numbers', 'ec'), ('taxonomyids', 'tax'), ('gene_names', 'gene'), ('go_terms', 'go')]
 
 parser = argparse.ArgumentParser(description='Merge annotations.')
 parser.add_argument('dataset_file', metavar='DATASET_FILE', help='.genes.json.gz input file')
@@ -109,7 +112,7 @@ def read_annotation(path, label, target_map):
       geneid = row[0][10:]
       if not geneid in target_map:
         target_map[geneid] = []
-      if int(row[3]) >= MIN_ALIGNMENT_LENGTH and len(target_map[geneid]) < NUMBER_OF_TOP_BLAST_HITS:
+      if int(row[3]) >= MIN_ALIGNMENT_LENGTH and len(target_map[geneid]) < NUMBER_OF_TOP_HITS_TO_KEEP:
         target_map[geneid].append({
           'id': row[1],
           'ident': float(row[2]),
@@ -127,29 +130,108 @@ def read_annotation(path, label, target_map):
   print('Got %s entries after parsing %s rows.' % (len(target_map), row_count))
 
 
-def extend_gene_annotation(gene):
-  # PDB
+def extend_gene_annotation_pdb(gene):
   if gene['geneid'] in pdb_annotation_map:
     pdbs = pdb_annotation_map[gene['geneid']]
     for pdb in pdbs:
       try:
-        pdb['description'] = pdb_map[pdb['id'][:4]]
+        pdb['label'] = pdb_map[pdb['id'][:4]]
       except KeyError:
         pass
     gene['x_pdb'] = pdbs
+
+
+def extend_gene_annotation_tdm(gene):
   # 3DM
+  # {
+  #   "structure_count": 827,
+  #   "superfamily": {
+  #     "structure_count": 827,
+  #     "cached_seq_clusters": 24229,
+  #     "seq_count": 58943,
+  #     "seq_clusters": 26854,
+  #     "dbname": "nucleo_kinases_virusx_2016",
+  #     "core_length": 223,
+  #     "subfamily_count": 85,
+  #     "nice_name": "Nucleotide kinases (2016)",
+  #     "mutation_count": 6742,
+  #     "proteins_with_gene_names": 57398,
+  #     "ec_numbers": {
+  #       "2.7.4.14": "0.894",
+  #       "2.7.4.8": "12.570"
+  #     },
+  #     "taxonomyids": {
+  #       "35525": "0.532",
+  #       "9606": "0.331"
+  #     },
+  #     "proteins_with_ec_numbers": 1746,
+  #     "proteins_with_taxonomy_ids": 73344,
+  #     "gene_names": {
+  #       "gmk": "16.997",
+  #       "coaE": "14.499"
+  #     },
+  #     "go_terms": {
+  #       "GO:0015937": "4.504",
+  #       "GO:0008331": "2.632"
+  #     },
+  #     "proteins_with_go_terms": 487
+  #   },
+  #   "cached_seq_clusters": 24229,
+  #   "seq_count": 58943,
+  #   "seq_clusters": 26854,
+  #   "dbname": "nucleo_kinases_virusx_2016",
+  #   "core_length": 223,
+  #   "subfamily_count": 85,
+  #   "nice_name": "Nucleotide kinases (2016)",
+  #   "mutation_count": 6742,
+  #   "proteins_with_gene_names": 1004,
+  #   "ec_numbers": {
+  #     "2.7.4.8": "100.000"
+  #   },
+  #   "taxonomyids": {
+  #     "1408": "0.480",
+  #     "1280": "2.305"
+  #   },
+  #   "proteins_with_ec_numbers": 23,
+  #   "proteins_with_taxonomy_ids": 1041,
+  #   "gene_names": {
+  #     "gmk": "97.908",
+  #     "gmk46": "0.100"
+  #   },
+  #   "go_terms": {
+  #     "GO:0005829": "50.000",
+  #     "GO:0004385": "50.000"
+  #   },
+  #   "proteins_with_go_terms": 4
+  # }
   if gene['geneid'] in tdm_annotation_map:
     tdms = tdm_annotation_map[gene['geneid']]
     for tdm in tdms:
-      try:
-        if 'superfamily' in tdm_map[tdm['id']]:
-          pass  # TODO put both (merge?)
-        else:
-          pass  # TODO put single
-      except KeyError:
-        print('ERR:', tdm['id'])
+      ref = tdm_map[tdm['id']]
+      # single value fields
+      for source_field, target_field in TDM_SINGLE_FIELD_MAP:
+        tdm[target_field] = ref[source_field]
+      # multi value maps
+      for source_field, target_field in TDM_MULTI_FIELD_MAP:
+        tdm[target_field] = []
+        if ref[source_field]:
+          for k, v in ref[source_field].items():
+            tdm[target_field].append({'id': k, 'perc': float(v)})
+        elif 'superfamily' in ref:
+          for k, v in ref['superfamily'][source_field].items():
+            tdm[target_field].append({'id': k, 'perc': float(v)})
     gene['x_3dm'] = tdms
+
+
+def deduplicate_ecs(gene):
+  gene['ecs'] = sorted(list(set(gene['ecs'])))
+
+
+def extend_gene_annotation(gene):
+  extend_gene_annotation_pdb(gene)
+  extend_gene_annotation_tdm(gene)
   # TODO: pfam
+  deduplicate_ecs(gene)
   return gene
 
 
